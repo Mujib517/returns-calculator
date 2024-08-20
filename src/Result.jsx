2d8b59e2fef5e84c815681ad15e5c55a8556022e
Mujib <mujib@Mujibs-MacBook-Pro-2.local>
2024-08-20 15:28:19 +0530

    format

--- src/Result.jsx
+++ src/Result.jsx
@@ -4,9 +4,9 @@ function Result({ data }) {
     const { amount, finalAmount, cagr, tenure } = data;
 
     return (<div>
-        <h1 className="mb-2 font-bold text-gray-500">Invested {amount.toLocaleString('en-in', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</h1>
-        <h1 className="mb-2 font-bold text-gray-500">Returns {(finalAmount - amount).toLocaleString('en-in', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} </h1>
-        <h1 className="font-bold text-2xl text-gray-600">Total {finalAmount.toLocaleString('en-in', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} </h1>
+        <h1 className="mb-2 font-bold text-gray-500">Invested {amount.toLocaleString('en-us', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</h1>
+        <h1 className="mb-2 font-bold text-gray-500">Returns {(finalAmount - amount).toLocaleString('en-us', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} </h1>
+        <h1 className="font-bold text-2xl text-gray-600">Total {finalAmount.toLocaleString('en-us', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} </h1>
         <span className="mb-4 font-semibold text-xs text-gray-400">@ {cagr}% for {tenure} years</span>
         <PieChart data={data} />
     </div>);
