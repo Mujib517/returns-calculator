a67b8a686cba292c3bb6b6059661e3144d3d081f
Mujib <mujib@Mujibs-MacBook-Pro-2.local>
2024-08-21 15:46:53 +0530

    Format3

--- src/Result.jsx
+++ src/Result.jsx
@@ -15,13 +15,13 @@ function Result({ data }) {
         <h1 className="text-2xl font-bold text-center text-gray-800 mb-8">Summary</h1>
         <div className="space-y-4">
             {/* Invested Amount */}
-            <div className="flex items-center justify-between bg-blue-100 text-blue-800 px-4 py-2 rounded-md">
+            <div className="flex items-center justify-between bg-blue-100 text-blue-800 px-4 py-4 rounded-md">
                 <span className="text-lg font-medium">Invested Amount</span>
                 <span className="text-lg font-semibold">{getFormattedCurrency(amount)}</span>
             </div>
 
             {/* Returns */}
-            <div className="flex items-center justify-between bg-green-100 text-green-800 px-4 py-2 rounded-md">
+            <div className="flex items-center justify-between bg-green-100 text-green-800 px-4 py-4 rounded-md">
                 <span className="text-lg font-medium">Expected Returns</span>
                 <span className="text-lg font-semibold">{getFormattedCurrency(returns)}</span>
             </div>
